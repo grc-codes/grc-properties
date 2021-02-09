d983e511b238eb5ade6836148aa731ddfbd8f466
Andrew Garcia <grc.codes@gmail.com>
2021-02-08 19:47:57 -0500

    created mobile manager menu

--- resources/views/includes/manager-navbar.blade.php
+++ resources/views/includes/manager-navbar.blade.php
@@ -1,5 +1,5 @@
-<div class='header-nav'>
-    <nav class='navbar navbar-expand-lg fixed-top bg-secondary bg-gradient'>
+<div id='header-nav'>
+    <nav class='navbar navbar-expand-lg fixed-top navbar-dark bg-gradient'>
         <div class='container-fluid px-4'>
             <a href='/' class='navbar-brand'>GRC Manager Dashboard</a>
         </div>
